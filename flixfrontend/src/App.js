86f8dfee905c53a4966364b2599bbfc5ed405c58
Riyakumarint <riyakumaribkj@gmail.com>
2020-10-11 09:57:48 +0530

    logo

--- flixfrontend/src/App.js
+++ flixfrontend/src/App.js
@@ -5,13 +5,20 @@ import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
 import './App.css';
 function App() {
   return  (
-
-    <Switch>
-      <Route path="/">
-    <Header />
-    <Home />
-    </Route>
-</Switch>
+    <Router>
+      <div className="app">
+       
+        
+        <Switch>
+        <Route path="/">
+       <Header />
+       <Home />
+      </Route>
+        </Switch>
+    
+        </div>
+    </Router>
+    
     
     );
   }
--- flixfrontend/src/Home.js
+++ flixfrontend/src/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import "./Home.css";
 function Home() {
     return (
-        <div>
-            
+        <div className ="home">
+            <div className="home__container">
+                
+                <img 
+                className='home__image'
+                src="https://cutewallpaper.org/21/dr-strange-logo/Download-Free-png-Doctor-Strange-Logo-Png-109-images-in-.png" 
+                alt=""
+                />
+    </div>
         </div>
-    )
+    );
 }
 
 export default Home;
