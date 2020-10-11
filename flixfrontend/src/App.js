59cb77b0db8773c38d4ccfbca3531d32cc12c58c
Riyakumarint <riyakumaribkj@gmail.com>
2020-10-11 09:33:29 +0530

    HOME

--- flixfrontend/src/App.js
+++ flixfrontend/src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
-
+import Home from './Home';
+import Header from './Header';
+import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
 import './App.css';
-
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Home, Browse, SignIn, SignUp } from './page';
-import * as ROUTES from './constants/routes';
-import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
-import { useAuthListener } from './hooks';
-
 function App() {
-  const { user } = useAuthListener();
+  return  (
 
-  return (
-    <Router>
-      <Switch>
-        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
-          <SignIn />
-        </IsUserRedirect>
-        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
-          <SignUp />
-        </IsUserRedirect>
-        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
-          <Browse />
-        </ProtectedRoute>
-        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
-          <Home />
-        </IsUserRedirect>
-      </Switch>
-    </Router>
-  );
-}
-
+    <Switch>
+      <Route path="/">
+    <Header />
+    <Home />
+    </Route>
+</Switch>
+    
+    );
+  }
 export default App;
